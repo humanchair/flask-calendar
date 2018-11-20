6d70e05e3359ef0e82564ea2cb0d01344ff276b6
Kunyuan Ma <kunyuan.ma@oracle.com>
2018-11-20 19:42:29 +0200

    Add "Customized" repeat option and some changes
    Add "Customized" repeat option and some changes

--- calendar_data.py
+++ calendar_data.py
@@ -1,25 +1,33 @@
+import datetime
+from copy import deepcopy
 from typing import cast, Dict, List, Optional
 import json
 import os
 import time
 
-
 from gregorian_calendar import GregorianCalendar
-
 
 KEY_TASKS = "tasks"
 KEY_USERS = "users"
 KEY_NORMAL_TASK = "normal"
 KEY_REPETITIVE_TASK = "repetition"
 KEY_REPETITIVE_HIDDEN_TASK = "hidden_repetition"
+KEY_REPEAT_BEGIN = "customized_repeat_begin"
+KEY_REPEAT_VALUE = "repetition_value"
+KEY_REPEAT_TOTAL = "customized_total_repeat"
 
 
 class CalendarData:
-
     REPETITION_TYPE_WEEKLY = "w"
     REPETITION_TYPE_MONTHLY = "m"
     REPETITION_SUBTYPE_WEEK_DAY = "w"
     REPETITION_SUBTYPE_MONTH_DAY = "m"
+    # customized type
+    REPETITION_TYPE_CUSTOMIZED = "c"
+
+    # FROM DAY : x
+    # repeat every x days
+    # total repeats : x (total)
 
     def __init__(self, data_folder: str) -> None:
         self.data_folder = data_folder
@@ -107,7 +115,9 @@ class CalendarData:
 
         task = [task for task in data[KEY_TASKS][KEY_REPETITIVE_TASK] if task["id"] == task_id][0]  # type: Dict
         task["repeats"] = True
-        task["date"] = self.date_for_frontend(year=year, month=month, day=1)
+        task["date"] = self.date_for_frontend(year=year, month=month,
+                                              day=int(str(task[KEY_REPEAT_BEGIN]).split("-")[2]))
+        # task["date"] = self.date_for_frontend(year=year, month=month, day=1)
         return task
 
     @staticmethod
@@ -115,7 +125,7 @@ class CalendarData:
         return "{0}-{1:02d}-{2:02d}".format(int(year), int(month), int(day))
 
     def add_repetitive_tasks_from_calendar(
-        self, year: int, month: int, data: Dict, tasks: Dict, view_past_tasks: Optional[bool] = True
+            self, year: int, month: int, data: Dict, tasks: Dict, view_past_tasks: Optional[bool] = True
     ) -> Dict:
         current_day, current_month, current_year = GregorianCalendar.current_date()
 
@@ -163,7 +173,7 @@ class CalendarData:
                 if task["id"] == task_id:
                     data[KEY_TASKS][KEY_REPETITIVE_TASK].pop(index)
                     if str(task_id) in data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK]:
-                        del(data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK][str(task_id)])
+                        del (data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK][str(task_id)])
 
         self._save_calendar(contents=data, filename=calendar_id)
 
@@ -204,7 +214,52 @@ class CalendarData:
                 return False
             new_task["repetition_type"] = repetition_type
             new_task["repetition_subtype"] = repetition_subtype
-            new_task["repetition_value"] = repetition_value
+            new_task[KEY_REPEAT_VALUE] = repetition_value
+            data[KEY_TASKS][KEY_REPETITIVE_TASK].append(new_task)
+        else:
+            if year is None or month is None or day is None:
+                return False
+            year_str = str(year)
+            month_str = str(month)
+            day_str = str(day)
+            if year_str not in data[KEY_TASKS][KEY_NORMAL_TASK]:
+                data[KEY_TASKS][KEY_NORMAL_TASK][year_str] = {}
+            if month_str not in data[KEY_TASKS][KEY_NORMAL_TASK][year_str]:
+                data[KEY_TASKS][KEY_NORMAL_TASK][year_str][month_str] = {}
+            if day_str not in data[KEY_TASKS][KEY_NORMAL_TASK][year_str][month_str]:
+                data[KEY_TASKS][KEY_NORMAL_TASK][year_str][month_str][day_str] = []
+            data[KEY_TASKS][KEY_NORMAL_TASK][year_str][month_str][day_str].append(new_task)
+
+        self._save_calendar(contents=data, filename=calendar_id)
+        return True
+
+    # create customized repeated tasks
+    def create_customized_task(self, calendar_id: str, year: Optional[int], month: Optional[int], day: Optional[int],
+                               title: str,
+                               is_all_day: bool, due_time: str, details: str, color: str, has_repetition: bool,
+                               repetition_type: str, repetition_value: int, customized_total_repeat: int) -> bool:
+        details = details if len(details) > 0 else "&nbsp;"
+        data = self.load_calendar(calendar_id)
+
+        new_task = {
+            "id": int(time.time()),
+            "color": color,
+            "due_time": due_time,
+            "is_all_day": is_all_day,
+            "title": title,
+            "details": details
+        }
+        if has_repetition:
+            # if repetition_type == self.REPETITION_SUBTYPE_MONTH_DAY and repetition_value == 0:
+            #     return False
+            new_task["repetition_type"] = repetition_type
+            # new_task["repetition_subtype"] = repetition_subtype
+            new_task[KEY_REPEAT_VALUE] = repetition_value
+
+            # if repetition_type == self.REPETITION_TYPE_CUSTOMIZED:
+            new_task["customized_repeat_value"] = repetition_value
+            new_task[KEY_REPEAT_TOTAL] = customized_total_repeat
+            new_task[KEY_REPEAT_BEGIN] = str(year) + "-" + str(month) + "-" + str(day)
             data[KEY_TASKS][KEY_REPETITIVE_TASK].append(new_task)
         else:
             if year is None or month is None or day is None:
@@ -238,7 +293,7 @@ class CalendarData:
         self._save_calendar(contents=data, filename=calendar_id)
 
     def _repetitive_tasks_from_calendar(
-        self, year: int, month: int, month_days: List, calendar_id: Optional[str] = None, data: Dict = None
+            self, year: int, month: int, month_days: List, calendar_id: Optional[str] = None, data: Dict = None
     ) -> Dict:
         if data is None:
             if calendar_id is None:
@@ -258,26 +313,60 @@ class CalendarData:
         for task in data[KEY_TASKS][KEY_REPETITIVE_TASK]:
             id_str = str(task["id"])
             monthly_task_assigned = False
+
             for week in month_days:
                 for weekday, day in enumerate(week):
                     if day == 0:
                         continue
+
+                    total_repeats = str(task[KEY_REPEAT_TOTAL])
+                    repeat_freq = str(task[KEY_REPEAT_VALUE])
+                    this_day_date = datetime.date(year, month, day)
+                    begin_year, begin_month, begin_day = str(task[KEY_REPEAT_BEGIN]).split("-")
+                    begin_day_date = datetime.date(int(begin_year), int(begin_month), int(begin_day))
+                    days_range_timedelta = this_day_date - begin_day_date
+
+                    # if this day before the begin day, pass
+                    if int(days_range_timedelta.days) < 0:
+                        continue
+
                     if task["repetition_type"] == self.REPETITION_TYPE_WEEKLY:
                         if self._is_repetition_hidden_for_day(data, id_str, year_str, month_str, str(day)):
                             continue
-                        if task["repetition_value"] == weekday:
-                            self.add_task_to_list(repetitive_tasks, day, task)
+                        # as this is weekly, so should be
+                        if (int(days_range_timedelta.days) % (int(repeat_freq) * 7) == 0) and (
+                                int(days_range_timedelta.days) / (int(repeat_freq) * 7) < int(total_repeats)):
+                            task_with_count = deepcopy(task)
+                            task_with_count["title"] = task["title"] + "(" + str(
+                                int(days_range_timedelta.days / (int(repeat_freq) * 7) + 1)) \
+                                                       + "/" + total_repeats + ")"
+                            self.add_task_to_list(repetitive_tasks, day, task_with_count)
                     elif task["repetition_type"] == self.REPETITION_TYPE_MONTHLY:
                         if self._is_repetition_hidden(data, id_str, year_str, month_str):
                             continue
-                        if task["repetition_subtype"] == self.REPETITION_SUBTYPE_WEEK_DAY:
-                            if task["repetition_value"] == weekday and not monthly_task_assigned:
-                                monthly_task_assigned = True
-                                self.add_task_to_list(repetitive_tasks, day, task)
-                        else:
-                            if task["repetition_value"] == day:
-                                self.add_task_to_list(repetitive_tasks, day, task)
-
+                        if day == int(begin_day) \
+                                and (month + (12 * (year - int(begin_year)))) >= int(begin_month) \
+                                and year >= int(begin_year):
+                            if (month - int(begin_month) + (12 * (year - int(begin_year)))) % int(repeat_freq) == 0 \
+                                    and (month - int(begin_month) + (12 * (year - int(begin_year)))) / int(
+                                repeat_freq) < int(total_repeats):
+                                task_with_count = deepcopy(task)
+                                task_with_count["title"] = task["title"] + "(" + str(
+                                    int((month + (12 * (year - int(begin_year)))) - int(begin_month) + 1)) + "/" + total_repeats + ")"
+                                self.add_task_to_list(repetitive_tasks, day, task_with_count)
+                    elif task["repetition_type"] == self.REPETITION_TYPE_CUSTOMIZED:
+                        if self._is_repetition_hidden_for_day(data, id_str, year_str, month_str, str(day)):
+                            continue
+                        if (KEY_REPEAT_TOTAL in task.keys()) and (KEY_REPEAT_BEGIN in task.keys()):
+                            # as there is a begin day, so we only check the future, not the past
+                            # if int(days_range_timedelta.days) >= 0:
+                            if (int(days_range_timedelta.days) % int(repeat_freq) == 0) and (
+                                    int(days_range_timedelta.days) / int(repeat_freq) < int(total_repeats)):
+                                task_with_count = deepcopy(task)
+                                task_with_count["title"] = task["title"] + "(" + str(
+                                    int(days_range_timedelta.days / int(repeat_freq) + 1)) \
+                                                           + "/" + total_repeats + ")"
+                                self.add_task_to_list(repetitive_tasks, day, task_with_count)
         return repetitive_tasks
 
     @staticmethod
@@ -315,16 +404,16 @@ class CalendarData:
                     if len(data[KEY_TASKS][KEY_NORMAL_TASK][year][month][day]) == 0:
                         days_to_delete.append(day)
                 for day in days_to_delete:
-                    del(data[KEY_TASKS][KEY_NORMAL_TASK][year][month][day])
+                    del (data[KEY_TASKS][KEY_NORMAL_TASK][year][month][day])
                 if len(data[KEY_TASKS][KEY_NORMAL_TASK][year][month]) == 0:
                     months_to_delete.append(month)
             for month in months_to_delete:
-                del(data[KEY_TASKS][KEY_NORMAL_TASK][year][month])
+                del (data[KEY_TASKS][KEY_NORMAL_TASK][year][month])
             if len(data[KEY_TASKS][KEY_NORMAL_TASK][year]) == 0:
                 years_to_delete.append(year)
 
         for year in years_to_delete:
-            del(data[KEY_TASKS][KEY_NORMAL_TASK][year])
+            del (data[KEY_TASKS][KEY_NORMAL_TASK][year])
 
     @staticmethod
     def _clear_past_hidden_entries(data: Dict) -> None:
@@ -339,13 +428,13 @@ class CalendarData:
                     if (int(year) < current_year) or (int(year) == current_year and int(month) < current_month):
                         months_to_delete.append(month)
                 for month in months_to_delete:
-                    del(data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK][task_id][year][month])
+                    del (data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK][task_id][year][month])
                 if len(data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK][task_id][year]) == 0:
                     years_to_delete.append(year)
             for year in years_to_delete:
-                del(data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK][task_id][year])
+                del (data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK][task_id][year])
             if len(data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK][task_id]) == 0:
                 task_ids_to_delete.append(task_id)
 
         for task_id in task_ids_to_delete:
-            del(data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK][task_id])
+            del (data[KEY_TASKS][KEY_REPETITIVE_HIDDEN_TASK][task_id])
